fcb1b9eb24c05aad1406388394de9a3672f91342
philliplumod <philliplumod@gmail.com>
2023-07-11 14:15:18 +0800

    generated named route in bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:counter_app/logic/cubit/counter_cubit.dart';
-import 'package:counter_app/presentation/screens/home_screen.dart';
-import 'package:counter_app/presentation/screens/second_screen.dart';
-import 'package:counter_app/presentation/screens/third_screen.dart';
+import 'package:counter_app/logic/router/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,7 +8,6 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-
   const MyApp({super.key});
 
   @override
@@ -18,52 +15,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final CounterCubit _counterCubit = CounterCubit();
+  final AppRouter _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => CounterCubit(),
       child: MaterialApp(
-        routes: {
-          '/': (context) => BlocProvider.value(
-            value: _counterCubit,
-            child: const HomeScreen(
-                  title: 'Flutter Demo Home Page',
-                  color: Colors.blueAccent,
-                ),
-          ),
-          '/second': (context) => BlocProvider.value(
-            value: _counterCubit,
-            child: const SecondScreen(
-                  title: 'Second Screen',
-                  color: Colors.redAccent,
-                ),
-          ),
-          '/third': (context) => BlocProvider.value(
-            value: _counterCubit,
-            child: const ThirdScreen(
-                  title: 'Third Screen',
-                  color: Colors.greenAccent,
-                ),
-          ),
-        },
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity:
+              VisualDensity.adaptivePlatformDensity, // for responsive UI
+        ),
+        onGenerateRoute: _appRouter.onGenerateRoute,
       ),
     );
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
-    _counterCubit.close();
+    _appRouter.dispose();
     super.dispose();
   }
 }
-
-
-
-
-
 
 //#6 - BLoC Access & Navigation - Local, Route & Global Access with BlocProvider
 // https://www.youtube.com/watch?v=laqnY0NjU3M&list=PLptHs0ZDJKt_T-oNj_6Q98v-tBnVf-S_o&index=7
